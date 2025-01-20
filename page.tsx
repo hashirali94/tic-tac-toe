6fea238b41281c6de7d821bce6d9da63e2d5bac9
hashir ali <160639117+hashirali94@users.noreply.github.com>
2025-01-21 02:23:29 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import TicTacToe from "./components/TicTacToe"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <TicTacToe />
+    </main>
+  )
+}
+
+
+
